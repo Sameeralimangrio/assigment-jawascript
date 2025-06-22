49782a1f184d82078fbda96c6e95fa608d3b90b7
Sameeralimangrio <sameealimangrio@gmail.com>
2025-06-22 22:36:00 +0500

    Add files via upload

---
+++ Assignment1/app.js
@@ -0,0 +1,185 @@
+// Assignment 1
+// alert("Greetings! Welcome to our web page.");
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 2
+// document.write("Oops! Please provide a valid password.");
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 3
+// document.write("First line displayed here.<br>");
+// document.write("Second line displayed after break.");
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 4
+// alert("First Alert!");
+// alert("Second Alert!");
+// alert("Third Alert!");
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 5
+// ✅ Completed
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 6
+// ✅ Completed
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 7
+// let user;
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 8
+// let fullName = "Sameer ali";
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 9
+// let msg = "Hello World!";
+// alert(msg);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 10
+// let name = "Sameer ali";
+// let age = 20;
+// let course = "Web Development";
+// let rollNo = "a-004";
+
+// alert("Student Bio");
+// alert(name);
+// alert(age);
+// alert(course);
+// alert(rollNo);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 11
+// let myEmail = "abc@gmail.com";
+// alert("My email address is: " + myEmail);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 12
+// document.write("Hello, World!");
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 13
+// let bar = "▬▬▬▬▬▬▬▬▬";
+// alert(bar);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 14
+// let myAge = 20;
+// alert("I am " + myAge + " years old.");
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 15
+// Not completed
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 16
+// let birthYear = 2004;
+// document.write("My birth year is: " + birthYear);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 17
+// let buyerName = "Sameer ";
+// let item = "T-shirt";
+// let qty = 5;
+// let result = buyerName + " ordered " + qty + " " + item + "(s) from the clothing store.";
+// document.getElementById("orderMessage").innerText = result;
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 18
+// let greeting, number, flag;
+// greeting = "Hello";
+// number = 123;
+// flag = true;
+// console.log(greeting, number, flag);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 19
+// Legal Variable Names
+// let name1 = "mohsin";
+// let _id = 456;
+// let $price = 100;
+// let user_name = "User";
+// let MyVar = "yes";
+
+// Illegal Variable Names
+// let 123abc; // starts with number ❌
+// let user name; // space ❌
+// let let; // reserved word ❌
+// let my-name; // hyphen ❌
+// let !user; // special character ❌
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 20
+// document.write("<h3>Rules for Naming JavaScript Variables</h3>");
+// document.write("1. Variable names can include letters, numbers, $, and _.<br>");
+// document.write("2. Variables must begin with a letter, $, or _.<br>");
+// document.write("3. Variable names are case sensitive.<br>");
+// document.write("4. Variable names must not be reserved keywords.");
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 21
+// let number1 = 15;
+// let number2 = 5;
+// let total = number1 + number2;
+// document.write("Sum of " + number1 + " and " + number2 + " is " + total);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 22
+// let a = 15, b = 5;
+
+// let add = a + b;
+// document.write("Addition: " + add + "<br>");
+
+// let subtract = a - b;
+// document.write("Subtraction: " + subtract + "<br>");
+
+// let multiply = a * b;
+// document.write("Multiplication: " + multiply + "<br>");
+
+// let divide = a / b;
+// document.write("Division: " + divide + "<br>");
+
+// let mod = a % b;
+// document.write("Modulus: " + mod);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 23
+// let num;
+// document.write("Value after declaration is: " + num + "<br>");
+
+// num = 5;
+// document.write("Initial value: " + num + "<br>");
+
+// num++;
+// document.write("After increment: " + num + "<br>");
+
+// let added = num + 7;
+// document.write("After adding 7: " + added + "<br>");
+
+// added--;
+// document.write("After decrement: " + added + "<br>");
+
+// let rem = added % 3;
+// document.write("Remainder when divided by 3: " + rem);
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 24
+// let ticketCost = 600;
+// let totalTickets = 5;
+// let finalPrice = ticketCost * totalTickets;
+// document.write("Total cost of 5 movie tickets is: " + finalPrice + " PKR");
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Assignment 25
+// var tableNumber = +prompt("Please enter a number to generate its table:");
+
+// if (isNaN(tableNumber)) {
+//   document.write("Invalid input. Please enter a valid number.");
+// } else {
+//   document.write("<h3>Multiplication Table of " + tableNumber + "</h3>");
+//   for (var i = 1; i <= 10; i++) {
+//     document.write(tableNumber + " × " + i + " = " + (tableNumber * i) + "<br>");
+//   }
+// }
