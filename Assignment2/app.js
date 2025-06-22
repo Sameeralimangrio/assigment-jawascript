7707d0873bdc56a93c4a257079ae05ea03222a9f
Sameeralimangrio <sameealimangrio@gmail.com>
2025-06-22 22:36:27 +0500

    Add files via upload

---
+++ Assignment2/app.js
@@ -0,0 +1,57 @@
+// Assignment 1
+// let a = 10;
+
+// document.write("Initial value of a: " + a + "<br>");
+
+// a++;
+// document.write("After first increment (++a): " + a + "<br>");
+
+// a++;
+// document.write("After second increment (++a): " + a + "<br>");
+
+// a--;
+// document.write("After first decrement (--a): " + a + "<br>");
+
+// a--;
+// document.write("After second decrement (--a): " + a + "<br>");
+
+// Assignment 2
+// let a = 2, b = 1;
+// let result = --a - --b + ++b + b--;
+
+// document.write("Value of a: " + a + "<br>");
+// document.write("Value of b: " + b + "<br>");
+// document.write("Final result: " + result + "<br>");
+
+// Assignment 3
+// let userName = prompt("What is your name?");
+// document.write("Hi " + userName + "! Glad to have you here.");
+
+// Assignment 4
+// let number = +prompt("Enter a number to see its table:");
+
+// if (isNaN(number) || number === 0) {
+//     number = 5; // default if invalid
+// }
+
+// document.write("<h3>Multiplication Table of " + number + "</h3>");
+// for (let i = 1; i <= 10; i++) {
+//     document.write(number + " × " + i + " = " + (number * i) + "<br>");
+// }
+// Assignment 5
+// let subject1 = prompt("Enter Subject 1 name:");
+// let subject2 = prompt("Enter Subject 2 name:");
+// let subject3 = prompt("Enter Subject 3 name:");
+
+// const maxMarks = 100;
+// const totalMarks = 3 * maxMarks;
+
+// let marks1 = parseInt(prompt("Enter marks for " + subject1 + ":"));
+// let marks2 = parseInt(prompt("Enter marks for " + subject2 + ":"));
+// let marks3 = parseInt(prompt("Enter marks for " + subject3 + ":"));
+
+// let obtainedMarks = marks1 + marks2 + marks3;
+// let percentage = (obtainedMarks / totalMarks) * 100;
+
+// document.write("Total Marks Obtained: " + obtainedMarks + "<br>");
+// document.write("Percentage: " + percentage.toFixed(2) + "%");
